ee672ba55490b4a9f2cdc121cdddbfe8de23cb35
matierf <matierf2016@gmail.com>
2021-05-02 18:35:55 -0300

    Modificación .bat y eliminación de iniciador.h

--- PrimeraEntrega/iniciador.sh
+++
@@ -1,9 +0,0 @@
-flex ./Lexico.l
-bison -dyv ./Sintactico.y
-gcc lex.yy.c y.tab.c tabla_simbolos.c -o primera
-./primera.exe ./prueba.txt
-rm lex.yy.c
-rm y.tab.c
-rm y.output
-rm y.tab.h
-rm primera.exe
