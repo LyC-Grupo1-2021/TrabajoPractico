51a31dc966d87ab1207484d7e48c23ad008d59ad
matierf <matierf2016@gmail.com>
2021-05-03 13:34:42 -0300

    Se agregan Primera.exe y ts.txt

--- Grupo01/Iniciador.bat
+++ Grupo01/Iniciador.bat
@@ -2,10 +2,10 @@ flex Lexico.l
 pause
 bison -dyv ./Sintactico.y
 pause
-gcc lex.yy.c y.tab.c tabla_simbolos.c -o primera.exe
+gcc lex.yy.c y.tab.c tabla_simbolos.c -o Primera.exe
 pause
 pause
-primera.exe prueba.txt
+Primera.exe prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
