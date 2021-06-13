c86b1316a00ca437502b169f986aa0c61c2427e9
matierf <matierf2016@gmail.com>
2021-06-13 08:55:05 -0300

    Modificacion .bat con Graphviz

--- Grupo01/Iniciador.bat
+++ Grupo01/Iniciador.bat
@@ -2,12 +2,16 @@ flex Lexico.l
 pause
 bison -dyv ./Sintactico.y
 pause
-gcc lex.yy.c y.tab.c tabla_simbolos.c -o Primera.exe
+gcc lex.yy.c y.tab.c tabla_simbolos.c arbol_sintactico/arbol_sintactico.c -o Primera.exe
 pause
 pause
 Primera.exe prueba.txt
+pause
+dot -Tpng gragh.dot -o gragh.png
+pause
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
+del gragh.dot
 pause
